3ae7e0bdd1b60c524999ee0015ea3deba0e2404e
Hudson Leite Ramalho <hudson.desenv@gmail.com>
2021-01-04 12:21:04 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/ProjetoSemanaDevsuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSemanaDevsuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSemanaDevsuperiorApplication.class, args);
+	}
+
+}
