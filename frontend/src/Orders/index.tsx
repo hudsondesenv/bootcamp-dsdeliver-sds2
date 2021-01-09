99dea7e35f2667f5de303d7732e277d03dc9e64d
Hudson Leite Ramalho <hudson.desenv@gmail.com>
2021-01-09 18:53:15 -0300

    Finalizando aula 1/2

--- frontend/src/Orders/index.tsx
+++ frontend/src/Orders/index.tsx
@@ -2,11 +2,28 @@
 import './styles.css';
 
 import StepesHeader from './StepsHeader';
+import ProdutctsList from './ProductsList';
+
+import { useEffect, useState } from 'react';
+
+import { Product } from './types';
+import { FechProdutcs } from '../Api';
 
 function Orders(){
+
+const [products, setProduct] = useState<Product[]>([]);
+
+
+useEffect(() => {   
+    FechProdutcs()
+    .then(response => setProduct(response.data))
+    .catch(error => console.log(error))
+}, []);
+
     return (
        <div className="orders-container">
            <StepesHeader />
+           <ProdutctsList products={products}/>
        </div>
     )
 }
---
+++ frontend/src/Orders/types.ts
@@ -0,0 +1,8 @@
+export type Product = {
+    id: number;
+    name: string;
+    prince: number;
+    description: string;
+    imageUrl: string;
+
+}
